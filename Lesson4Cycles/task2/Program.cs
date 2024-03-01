f7be26d3699f34db724b9f4ff9ed364330b8b66d
Nilskih <145379206+Nilskih@users.noreply.github.com>
2024-03-01 23:13:49 +0300

    Add files via upload

---
+++ Lesson4Cycles/task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {/* DO WHILE. 
+      * Осуществить ввод последовательности целых чисел и сравнить, что больше,
+      * количество положительных или количество отрицательных. 
+      * Последовательность потенциально не ограничена, окончанием последовательности служит число 0.*/
+        static void Main(string[] args)
+        {
+            int number;
+            int CountPozitive = 0;
+            int CountNegative = 0;
+            Console.WriteLine("вводите числа");
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number > 0)
+                {
+                    CountPozitive++;
+                }
+                else
+                {
+                    CountNegative++;
+                }
+            } while (number != 0);
+            if (CountPozitive > CountNegative)
+            {
+                Console.WriteLine("положительных чисел больше");
+            }
+            else
+            {
+                Console.WriteLine(" отрицательных чисел больше");
+            }
+
+
+
+        }
+    }
+}
---
+++ Lesson4Cycles/task3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести положительные числа A, B, C. 
+             * На прямоугольнике размера A x B размещено максимально возможное количество квадратов со стороной C (без наложений).
+             * Найти количество квадратов, размещенных на прямоугольнике. Операции умножения и деления не использовать.*/
+
+            Console.Write("Введите A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("Введите сторону квадрата C: ");
+            int C = int.Parse(Console.ReadLine());
+            // Проверка, что квадрат со стороной C может быть размещен целиком в прямоугольнике, а то я вас знаю
+            if (C > A || C > B)
+            {
+                Console.WriteLine("Квадрат со стороной C не может быть размещен целиком в прямоугольнике");
+                return;
+            }
+            int count = 0; //счетчик кол-ва квадратов
+            int result = 0;
+            if (A > B) 
+            {
+                int Ac = A;// переменная для расчетов размещения по ширине
+                while (Ac >= C)
+                {
+                    Ac = Ac - C; // сколько раз сторона квадрата С  поместится в длину A
+                    count++;
+                }
+                result = count * (B / C); // тут дополнительно определяем, позволяет ли ширина прямоугольника разместить еще квадраты
+            }
+            else
+            {
+                int Bc = B; // переменная для расчетов размещения по ширине
+                while (Bc >= C)
+                {
+                    Bc = Bc - C; // сколько раз сторона квадрата С  поместится в длину B
+                    count++;
+                }
+                result = count * (A / C);
+            }
+            Console.WriteLine("Количество квадратов: " + result);
+        }
+    }
+}
+
